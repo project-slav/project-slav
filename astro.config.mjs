ad2653a2785292803ab09860fb3ca78badfc98ba
renetet <renetet@proton.me>
2024-06-17 19:53:00 +0200

    fix pth

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import config from "./src/config/config.json";
 
 // https://astro.build/config
 export default defineConfig({
-  site: config.site.base_url ? config.site.base_url : "https://reneet.github.io",
+  site: config.site.base_url ? config.site.base_url : "",
   base: config.site.base_path ? config.site.base_path : "/",
   trailingSlash: config.site.trailing_slash ? "always" : "never",
   image: {
